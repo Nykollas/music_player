a5e154981ce44b90f19552fb9d53fadc7c5e8653
Nykollas <nicolauarti09@gmail.com>
2020-09-09 23:37:22 -0300

    sidemenu component added

---
+++ src/components/sidemenu/SideMenu.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function SideMenu(props) {
+    return (
+        <div className='sidemenu'>
+            <div className='sidemenu--header'>
+                
+            </div>
+            <div className='sidemenu--options_container'>
+
+            </div>
+            <p className='sidemenu--options_title'>My Music</p>
+            <div className='sidemenu--options_container'>
+
+            </div>
+        </div>
+    )
+}
+
+SideMenu.propTypes = {
+
+}
+
+export default SideMenu
+
---
+++ src/pages/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Home(props) {
+    return (
+        <div className='main'>
+            <div className='row'>
+                <SideMenu></SideMenu>
+            </div>
+        </div>
+    )
+}
+
+Home.propTypes = {
+
+}
+
+export default Home
+
