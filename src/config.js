d1c733a58ba069d0c49249eac5ad58ce193a428a
Nykollas <nicolauarti09@gmail.com>
2020-09-28 11:11:57 -0300

    fix domain

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@
 export default {
     client_id:'0115ae117a764f7bb64a8de84b421713',
     client_secret:'eb1e22436df34d44890dab16b5acc1e8',
-    redirect_uri:'https://localhost:3000/callback',
+    redirect_uri:'https://music-player.nykollas.vercel.app/callback',
+    domain:'https://music-player.nykollas.vercel.app/'
 }
